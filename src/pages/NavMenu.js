dc62c2b64c1342bd780760e934fe28096420ab0d
sauravmodi <sauravmodi03@gmail.com>
2023-04-11 12:34:07 -0500

    Updating landing page css

--- src/pages/NavMenu.js
+++ src/pages/NavMenu.js
@@ -12,11 +12,11 @@ function NavMenu() {
     return (
         <nav id="nav-link-wrap" className='nav-link-wrap -drop-shadow'>
             <Link id="nav-close-btn" to="#" onClick={closeNav}>Close</Link>
-            <Link activeClass="active" className="scale" to="home" spy={true} smooth={true} duration={500}>Home</Link>
-            <Link activeClass="active" className="scale" to="about" spy={true} smooth={true} duration={500}>About</Link>
-            <Link activeClass="active" className="scale" to="skills" spy={true} smooth={true} duration={500}>Skills</Link>
-            <Link activeClass="active" className="scale" to="work" spy={true} smooth={true} duration={500}>Work</Link>
-            <Link activeClass="active" className="scale" to="contact" spy={true} smooth={true} duration={500}>Contact</Link>
+            <Link activeClass="active" className="scale home-nav" to="home" spy={true} smooth={true} duration={500}>Home</Link>
+            <Link activeClass="active" className="scale about-nav" to="about" spy={true} smooth={true} duration={500}>About</Link>
+            <Link activeClass="active" className="scale skills-nav" to="skills" spy={true} smooth={true} duration={500}>Skills</Link>
+            <Link activeClass="active" className="scale work-nav" to="work" spy={true} smooth={true} duration={500}>Work</Link>
+            <Link activeClass="active" className="scale contact-nav" to="contact" spy={true} smooth={true} duration={500}>Contact</Link>
         </nav>
     );
 }
