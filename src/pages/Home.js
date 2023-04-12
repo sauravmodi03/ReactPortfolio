770352517a14f4937d2c775dc89fffa285fbb202
sauravmodi <sauravmodi03@gmail.com>
2023-04-11 19:10:44 -0500

    Updating landing page css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -43,13 +43,13 @@ function Home() {
     return (
         <div id="home" className="Home flex-top-padding">
             <article>
-                <div onClick={manageQR} className="qr-wrapper">
-                    <img id="qr-logo" src={QRlogo} alt="QR logo"/>
-                </div>
                 <section id="avatar-container" className="avatar-container">
                     {avatar ? <img id="avatar" className="avatar" src={manLogo} alt="Avatar Logo"/>
                      : <img id="resume" className="avatar" src={resumeqr} alt="Avatar Logo"/> }
                 </section>
+                <div onClick={manageQR} className="qr-wrapper">
+                    <img id="qr-logo" src={QRlogo} alt="QR logo"/>
+                </div>
                 <section className="info">
                     <div className='info-l1-wraper drop-heading'>
                         <span className='info-l2 info-bg big-heading'>Hi, I'm Saurav.</span>
